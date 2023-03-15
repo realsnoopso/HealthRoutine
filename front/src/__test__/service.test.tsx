3050e1f0c0e1c1d3ce9fe119db5c793db0d996ca
realsnoopso <real@snoop.so>
2023-03-15 20:52:49 +0900

    Complete making service logics

--- front/src/__test__/service.test.tsx
+++ front/src/__test__/service.test.tsx
@@ -1,8 +1,14 @@
-import { checkRecordIsDone } from '../../src/services/service';
+import { checkRecordIsDone, postRecord } from '../../src/services/service';
 
 test('checkRecordIsDone', async () => {
   expect(await checkRecordIsDone('7490e5a4-f69f-4b4b-9191-df9f330edd4c')).toBe(
     true
   );
   expect(await checkRecordIsDone('')).toBe(null);
+});
+
+test('checkCurrentWeightAndCount', async () => {
+  expect(
+    await postRecord({ routineId: 'ddd', weight: 2, count: 2, isDone: false })
+  ).toBe(true);
 });
--- front/src/services/service.tsx
+++ front/src/services/service.tsx
@@ -1,9 +1,45 @@
 import { Record } from '@src/types/index';
-import { getLastRecordFromAPI } from '@src/apis/records';
+import { getLastRecordFromAPI, postRecordToAPI } from '@src/apis/records';
 
 export async function checkRecordIsDone(routineId: Record['routineId']) {
   const lastRecord = await getLastRecordFromAPI(routineId);
   if (!lastRecord) return null;
   const { isDone } = lastRecord;
   return isDone;
+}
+
+export function getUnfinishedRoutines(routines: Record[]) {
+  // todo: 루틴 API 먼저 수정하기
+  return routines.filter((routine) => !routine.isDone);
+}
+
+export function stopExercise() {
+  // todo: 작성
+}
+
+export function checkCurrentWeightAndCount(window: Window) {
+  return (
+    window.localStorage.hasItem('currentWeight') &&
+    window.localStorage.hasItem('currentCount')
+  );
+}
+
+export function getCurrentWeightAndCount(window: Window) {
+  return {
+    currentWeight: window.localStorage.getItem('currentWeight'),
+    currentCount: window.localStorage.getItem('currentCount'),
+  };
+}
+
+export function postCurrentWeight(window: Window, currentWeight: number) {
+  window.localStorage.setItem('currentWeight', String(currentWeight));
+}
+
+export function postCurrentCount(window: Window, currentCount: number) {
+  window.localStorage.setItem('currentCount', String(currentCount));
+}
+
+export async function postRecord(record: Record) {
+  const response = await postRecordToAPI(record);
+  return response?.status === 200 ? true : false;
 }
